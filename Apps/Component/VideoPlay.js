8159121046fca531d2f9749bb48d168aaa3b449e
Bhavin <bhavin.parghi007@gmail.com>
2020-10-13 13:04:29 +0530

    cover and controll

--- Apps/Component/VideoPlay.js
+++ Apps/Component/VideoPlay.js
@@ -1,55 +1,66 @@
-
-import React, { useEffect, useState, Component } from 'react';
-import { StyleSheet, Dimensions, Platform, View } from 'react-native';
+import React, {useEffect, useState, Component} from 'react';
+import {StyleSheet, Dimensions, Platform, View} from 'react-native';
 import Video from 'react-native-video';
-const width = Dimensions.get("window").width;
-const height = Dimensions.get("window").height;
+const width = Dimensions.get('window').width;
+const height = Dimensions.get('window').height;
 
 export default class VideoPlayer extends Component {
-    constructor(props) {
-        super(props);
-        this.video = props.route.params.video
-        this.fileType = props.route.params.fileType
-    }
+  constructor(props) {
+    super(props);
+    this.video = props.route.params.video;
+    this.fileType = props.route.params.fileType;
+  }
 
-    render() {
-        console.log('file', Platform.OS == 'android' ? `file://${this.video.path}` : this.video.path);
-        return (
-            <View style={styles.container}>
-                <Video
-                    
-                    source={{ uri: Platform.OS == 'android' ? `file://${this.video.path}` : this.video.path }}
-                    onBuffer={(data) => { console.log(data); }}
-                    onError={(data) => { console.log(data); }}
-                    style={Platform.OS === "android" ? styles.videoContainerAndroid : styles.videoContainerIOS}
-                    resizeMode="contain"
-                />
-            </View>
-        );
-    }
+  render() {
+    let cover = this.video.cover;
+    return (
+      <View style={styles.container}>
+        <Video
+          source={{
+            uri:
+              Platform.OS == 'android'
+                ? `file://${this.video.path}`
+                : this.video.path,
+          }}
+          onBuffer={(data) => {
+            console.log(data);
+          }}
+          onError={(data) => {
+            console.log(data);
+          }}
+          style={
+            Platform.OS === 'android'
+              ? styles.videoContainerAndroid
+              : styles.videoContainerIOS
+          }
+          poster={cover}
+          resizeMode="contain"
+          controls={true}
+        />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: 'black',
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    videoContainerAndroid: {
-        height: "100%",
-        width: "100%"
-    },
-    videoContainerIOS: {
-        width: Dimensions.get('window').height,
-        height: Dimensions.get('window').width,
-        minWidth: Dimensions.get('window').height,
-        minHeight: Dimensions.get('window').width,
-        width: Dimensions.get('screen').height,
-        height: Dimensions.get('screen').width,
+  container: {
+    backgroundColor: 'black',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  videoContainerAndroid: {
+    height: '100%',
+    width: '100%',
+  },
+  videoContainerIOS: {
+    width: Dimensions.get('window').height,
+    height: Dimensions.get('window').width,
+    minWidth: Dimensions.get('window').height,
+    minHeight: Dimensions.get('window').width,
+    width: Dimensions.get('screen').height,
+    height: Dimensions.get('screen').width,
 
-        transform: [{ rotate: '90deg' }],
-    }
+    transform: [{rotate: '90deg'}],
+  },
 });
-
-
--- Apps/Container/AudioScreen.js
+++ Apps/Container/AudioScreen.js
@@ -1,62 +1,68 @@
-
-import React, { useEffect, useState } from 'react';
-import { SafeAreaView, Text, View, Platform } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {SafeAreaView, Text, View, Platform} from 'react-native';
 import Audio from '../Component/Audio';
-import MusicFiles, { RNAndroidAudioStore } from "react-native-get-music-files";
+import MusicFiles, {RNAndroidAudioStore} from 'react-native-get-music-files';
 
+export default function AudioScreen({navigation}) {
+  const [audio, setAudio] = useState([]);
 
+  useEffect(() => {
+    if (Platform.OS == 'android') {
+      RNAndroidAudioStore.getAll({
+        blured: true, // works only when 'cover' is set to true
+        artist: true,
+        duration: true, //default : true
+        genre: true,
+        title: true,
+        cover: true,
+        minimumSongDuration: 10000, // get songs bigger than 10000 miliseconds duration,
+        fields: [
+          'title',
+          'albumTitle',
+          'genre',
+          'lyrics',
+          'artwork',
+          'duration',
+        ], // for iOs Version
+      })
+        .then((tracks) => {
+          console.log('tracks', tracks);
+          setAudio(tracks);
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+    } else {
+      MusicFiles.getAll({
+        blured: true, // works only when 'cover' is set to true
+        artist: true,
+        duration: true, //default : true
+        genre: true,
+        title: true,
+        cover: true,
+        minimumSongDuration: 10000, // get songs bigger than 10000 miliseconds duration,
+        fields: [
+          'title',
+          'albumTitle',
+          'genre',
+          'lyrics',
+          'artwork',
+          'duration',
+        ], // for iOs Version
+      })
+        .then((tracks) => {
+          setAudio(tracks);
+        })
+        .catch((error) => {
+          console.log('error', error);
+          // catch the error
+        });
+    }
+  }, []);
 
-export default function AudioScreen({ navigation }) {
-    const [audio, setAudio] = useState([]);
-
-    useEffect(() => {
-
-        if (Platform.OS == 'android') {
-            RNAndroidAudioStore.getAll({
-                blured: true, // works only when 'cover' is set to true
-                artist: true,
-                duration: true, //default : true
-                cover: false, //default : true,
-                genre: true,
-                title: true,
-                cover: true,
-                minimumSongDuration: 10000, // get songs bigger than 10000 miliseconds duration,
-                fields: ['title', 'albumTitle', 'genre', 'lyrics', 'artwork', 'duration'] // for iOs Version
-            }).then(tracks => {
-                console.log('tracks',tracks);
-                setAudio(tracks)
-            }).catch(error => {
-                console.log('error', error);
-            })
-        } else {
-            
-            MusicFiles.getAll({
-                blured: true, // works only when 'cover' is set to true
-                artist: true,
-                duration: true, //default : true
-                genre: true,
-                title: true,
-                cover: true,
-                minimumSongDuration: 10000, // get songs bigger than 10000 miliseconds duration,
-                fields: ['title', 'albumTitle', 'genre', 'lyrics', 'artwork', 'duration'] // for iOs Version
-            }).then(tracks => { 
-                setAudio(tracks)
-            }).catch(error => {
-                console.log('error', error);
-                // catch the error
-            })
-        }
-
-
-    }, []);
-
-
-    return (
-        <SafeAreaView style={{ flex: 1, marginTop: 10 }}>
-            <Audio list={audio} navigation={navigation} />
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={{flex: 1, marginTop: 10}}>
+      <Audio list={audio} navigation={navigation} />
+    </SafeAreaView>
+  );
 }
-
-
-
